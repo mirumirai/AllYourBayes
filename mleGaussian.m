6a2b0f422f9461d47a33d31bd0a3c86ab5ab2b50
Jeffrey Chiou <jeffchiou@gmail.com>
2013-11-24 16:35:34 -0500

    Finish

--- mleGaussian.m
+++ mleGaussian.m
@@ -1,4 +1,12 @@
-function param = mleGaussian(data)
-% Calculates the 
-    for i = 1:length(data
+function param = mleGaussian(data,mu,sigma)
+% Calculates the maximum likelihood estimate for Gaussian data
+% Data can be a scalar or a vector
+% Mu
+    if isvector(x)
+        likelihood = (1/sqrt(2*pi*sigma^2)) * ...
+            exp(-((x-mu).^2)/(2*sigma^2));
+    else
+        likelihood = (1/sqrt(2*pi*sigma^2)) * ...
+            exp(-((x-mu)^2)/(2*sigma^2));
+    end
 end
