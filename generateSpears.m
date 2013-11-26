7330a989278c4aa5344ea112e0b75262b8eb30a8
Jeffrey Chiou <jeffchiou@gmail.com>
2013-11-25 23:37:28 -0500

    generateSpears Done

--- generateSpears.m
+++ generateSpears.m
@@ -1,4 +1,11 @@
-function [spears] = generateSpears(prior1)
-    % Generates spears with priors
-    prior2 = 1-prior1;
+function [spears] = generateSpears(prior1,nSpears)
+    % Generates nSpears number of spears with priors
+    % Note: prior2 = 1-prior1;
+    for i = 1:nSpears
+        if rand(1)<prior1
+            spears(i)=1;
+        else
+            spears(i)=2;
+        end
+    end            
 end
