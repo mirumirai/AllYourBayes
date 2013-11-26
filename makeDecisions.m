0eee2cec615ebfca1b2422148881799478c97203
Justin Frye <jmfrye@andrew.cmu.edu>
2013-11-25 23:52:26 -0500

    makeDecisions.m outputs params cell array

--- makeDecisions.m
+++ makeDecisions.m
@@ -1,9 +1,13 @@
 % Given an array of data, call the make decision function for each
 % element.  Output the resulting array of decisions.
-function [decisions] = makeDecisions(datas, type, mu, sigma, prior)
+function [decisions, allParams] = makeDecisions(datas, type, mu, sigma, prior)
     decisionAccum = zeros(1,length(datas));
+    paramAccum = cell(1,length(datas));
     for i=(1:length(datas))
-       decisionAccum(i) = makeDecision(datas(i), type, mu, sigma, prior);
+        [decision, params] = makeDecision(datas(i), type, mu, sigma, prior);
+        decisionAccum(i) = decision;
+        paramAccum{i} = params;
     end
-    decision = decisionAccum;
+    decisions = decisionAccum;
+    allParams = paramAccum;
 end
